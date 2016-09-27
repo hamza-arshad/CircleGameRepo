0caa1fbe0fd12d31b4aa73c3faff89b605ef80b6
NoumanShahid1258 <nouman.hassan.shahid@gmail.com>
2016-09-27 19:20:06 +0500

    Cirlce annimation

---
+++ Circle Game/Assets/Scripts/CircleController.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleController : MonoBehaviour {
+    [SerializeField]
+    GameObject filledCircle;
+    [SerializeField]
+    GameObject dottedCircle;
+
+    
+    GameObject game;
+    GameController controller;
+    SpriteRenderer renderer1;
+    SpriteRenderer renderer2;
+
+    bool pressed;
+    bool flag;
+    float speed = 1;
+
+	void Start () {
+        speed = Random.Range(0.5f, 2.0f);
+        pressed = false;
+        flag = false;
+        renderer1 = dottedCircle.GetComponent<SpriteRenderer>();
+        renderer2 = filledCircle.GetComponent<SpriteRenderer>();
+        renderer1.color = new Color(255,255,255,255);
+        renderer2.color = new Color(255, 255, 255, 255);
+        game = GameObject.Find("GameController");
+        controller = game.GetComponent<GameController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (pressed)
+        {
+
+            filledCircle.transform.localScale = Vector3.Lerp(filledCircle.transform.localScale, new Vector3(filledCircle.transform.localScale.x + 1, filledCircle.transform.localScale.y + 1, 1), Time.deltaTime * speed);
+            flag = true; 
+        }
+        if(!pressed && flag)
+        {
+            float fx = filledCircle.transform.localScale.x;
+            float dx = dottedCircle.transform.localScale.x;
+            float per = dx * 0.05F;
+            float minX = dx - per;
+            float maxX = dx + per;
+
+            if(fx>=minX && fx<= maxX)
+            {
+
+                Debug.Log("U Won");
+                SpriteRenderer renderer = filledCircle.GetComponent<SpriteRenderer>();
+                renderer.color = new Color(0f, 128f, 0f, 1f);
+                controller.destroy();
+                flag = false;
+
+            }
+            else
+            {
+                Debug.Log("U lose");
+            }
+             
+
+
+        }
+
+
+
+	
+	}
+
+   public void SetPressed(bool f)
+    {
+        pressed = f;
+
+    }
+
+    public void SetSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+
+
+    public void SetDottedScale(float x, float y, float z)
+    {
+        dottedCircle.transform.localScale = new Vector3(x, y, z);
+    }
+
+}
---
+++ Circle Game/Assets/Scripts/GameController.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    bool isDown = false;
+    [SerializeField]
+    GameObject CirclePrefab;
+    private CircleController currntController;
+
+    private float speed = 0;
+    private float x, y;
+    private float dx;
+    private float dy;
+    private GameObject obj;
+    private SpriteRenderer sp;
+    private float s;
+    private bool destroyFlag;
+    
+    // Use this for initialization
+    void Start () {
+        
+        Application.targetFrameRate = 60;
+        create();
+        sp = obj.GetComponentInChildren<SpriteRenderer>();
+        destroyFlag = false;
+    }
+
+    
+
+    public void destroy()
+    {
+
+        destroyFlag = true;
+
+    }
+
+    void create ()
+    {
+        speed = Random.Range(2.0F, 5.0F);
+        s = Random.Range(0.5F, 1F);
+        dx = Random.Range(-1.5F, 1.5F);
+        dy = Random.Range(-4F, 4F);
+        obj = Instantiate(CirclePrefab);
+        currntController = obj.GetComponent<CircleController>();
+        obj.transform.position = new Vector3(dx, dy, 1);
+        currntController.SetDottedScale(s,s,1);
+        currntController.SetSpeed(speed);
+
+
+
+    }
+
+
+    void Dop(float op)
+    {
+        sp.color = new Color(0f, 128f, 0f, op);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetMouseButtonDown(0) && !isDown)
+        {
+            isDown = true;
+            OnMouseDown();
+
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDown = false;
+            OnMouseUp();
+
+        }
+
+
+        if (destroyFlag)
+        {
+            float op = sp.color.a;
+            if (op == 0)
+            {
+                Object.Destroy(obj);
+                destroyFlag = false;
+            }
+            else
+            {
+                op--;
+                Dop(op);
+            }
+
+
+        }
+
+
+
+    }
+    void OnMouseDown()
+    {
+        currntController.SetPressed(true);
+    }
+    void OnMouseUp()
+    {
+        currntController.SetPressed(false);
+    }
+}
