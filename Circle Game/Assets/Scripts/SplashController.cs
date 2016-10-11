4709513d3d525924aec22e0ba4ff58b7e7291a52
Umair-Sheikh <Umairakhtar69@gmail.com>
2016-10-11 22:16:09 +0500

    Splash Screen Animation Added

--- Circle Game/Assets/Scripts/SplashController.cs
+++ Circle Game/Assets/Scripts/SplashController.cs
@@ -4,21 +4,18 @@ using UnityEngine.SceneManagement;
 using Soomla.Store;
 
 public class SplashController : MonoBehaviour {
-    int timmer;
-    int mute;
+    float timmer;
 	// Use this for initialization
 	void Start () {
         timmer = 0;
-        mute = PlayerPrefs.GetInt(Helpers.MUTE_KEY, 0);
+		int mute = PlayerPrefs.GetInt(Helpers.MUTE_KEY, 0);
         if(mute == 1)
         {
             AudioListener.volume = 0F;
-            PlayerPrefs.SetInt(Helpers.MUTE_KEY, 1);
         }
         else
         {
             AudioListener.volume = 1.0F;
-            PlayerPrefs.SetInt(Helpers.MUTE_KEY, 0);
         }
 
     }
@@ -26,10 +23,10 @@ public class SplashController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        timmer++;
-        if (timmer >= (3 * 60))
+		timmer += Time.deltaTime;
+        if (timmer >= 1.5f)
         {
-            SceneManager.LoadScene(Helpers.HOME_SCENE_INDEX);
+			SceneManager.LoadSceneAsync(Helpers.HOME_SCENE_INDEX);
         }
 	
 	}
